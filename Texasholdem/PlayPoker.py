bf434f23d5ef305b4b4d3a4440ac7b7168045b9a
Magnus Kirø <magnuskiro@gmail.com>
2011-09-02 11:14:43 +0200

    Fixed Cards.py. Runs without errors. But I get no output.

---
+++ Texasholdem/PlayPoker.py
@@ -0,0 +1,16 @@
+import Cards
+
+__author__ = 'Kiro'
+
+players = []
+deck = Cards.gen_52_shuffled_cards()
+
+def main():
+    shuffleDeck()
+    print("test")
+    return 0
+
+def shuffleDeck():
+    deck = Cards.shuffle_cards
+
+

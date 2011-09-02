cb70efaed174ec8d92f5444c378cb82515c52931
Magnus Kirø <magnuskiro@gmail.com>
2011-09-02 13:16:57 +0200

    Added def's and tried to get classes to work.

--- Texasholdem/PlayPoker.py
+++ Texasholdem/PlayPoker.py
@@ -1,20 +1,41 @@
 import Cards
+import Player
 
 __author__ = 'Kiro'
 
+numberOfPlayers = 2
 players = []
 deck = Cards.gen_52_shuffled_cards()
 
 def main():
-    shuffleDeck()
-    print("test")
+    print("starting")
+    deck = Cards.gen_52_shuffled_cards()
+    deck = shuffleDeck(deck)
+    print(deck)
+    initializePlayers()
+    print(players)
+    print("Initialization done")
     return 0
 
-def shuffleDeck(deck):
-    deck = Cards.shuffle_cards(deck)
+def shuffleDeck(d):
+    d = Cards.shuffle_cards(d, 4)
+    return d
+
+def initializePlayers():
+    for i in range(numberOfPlayers):
+        p = Player()
+        p.setPlayerNumber(i)
+        players.append(p)
+        print(players[0].getPlayerNumber)
+
+def dealCards():
+    for p in players:
+        p.setHand(deck.pop(0), deck.p(0))
 
 def doCardCheck():
     return 0
+
+main()
 
 deck = Cards.card_deck()
 hand1 = deck.deal_n_cards(7)
--- Texasholdem/Player.py
+++ Texasholdem/Player.py
@@ -1,20 +1,42 @@
 __author__ = 'kiro'
 
-hand = []
-action = [call, bet, fold]
+class Player:
+    hand = []
+    action = ["call", "bet", "fold"]
+    playerNumber = -1
 
-def setHand(carda, cardb):
-    hand.__add__(carda)
-    hand.__add__(cardb)
-    return 0
+    def __init__(self, cash, number, carda, cardb):
+        self.setStartCash(cash)
+        self.setPlayerNumber(self,number)
+        self.setHand(self, carda, cardb)
 
-def getHand():
-    return hand;
+    def setStartCash(self, amount):
+        self.cash = amount
 
-# hand probabilities should be dynamic and configured in some kind of config file. So vi can load 10 different playing styles easily.
-def getAction(prob):
-    if prob < 0.2: x =2
-    elif prob >=0.7: x=1
-    else: x=0
-    return action[x]
+    def setPlayerNumber(self, n):
+        self.playerNumber = n
+
+    def getPlayerNumber(self):
+        return self.playerNumber
+
+    def bet(self,amount):
+        self.cash -= amount
+
+    def addWinnings(self, amount):
+        self.cash += amount
+
+    def setHand(self, carda, cardb):
+        self.hand.__add__(carda)
+        self.hand.__add__(cardb)
+        return 0
+
+    def getHand(self):
+        return self.hand;
+
+    # hand probabilities should be dynamic and configured in some kind of config file. So vi can load 10 different playing styles easily.
+    def getAction(self, prob):
+        if prob < 0.2: x =2
+        elif prob >=0.7: x=1
+        else: x=0
+        return self.action[x]
 

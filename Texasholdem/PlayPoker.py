d3bd47b8eec57b10cf53660f511b44aae365baa9
Magnus Kirø <magnuskiro@gmail.com>
2011-09-02 12:09:41 +0200

    added som def's in player and more working stuff in playpoker

--- Texasholdem/PlayPoker.py
+++ Texasholdem/PlayPoker.py
@@ -10,11 +10,14 @@ def main():
     print("test")
     return 0
 
-def shuffleDeck():
-    deck = Cards.shuffle_cards(deck)
+def shuffleDeck(d):
+    d = Cards.shuffle_cards(d, 4)
+    return d
 
 def doCardCheck():
     return 0
 
 deck = Cards.gen_52_shuffled_cards()
 print("test")
+deck = shuffleDeck(deck)
+print(deck)
--- Texasholdem/Player.py
+++ Texasholdem/Player.py
@@ -1,2 +1,20 @@
-__author__ = 'dusken'
-  
+__author__ = 'kiro'
+
+hand = []
+action = [call, bet, fold]
+
+def setHand(carda, cardb):
+    hand.__add__(carda)
+    hand.__add__(cardb)
+    return 0
+
+def getHand():
+    return hand;
+
+# hand probabilities should be dynamic and configured in some kind of config file. So vi can load 10 different playing styles easily.
+def getAction(prob):
+    if prob < 0.2: x =2
+    elif prob >=0.7: x=1
+    else: x=0
+    return action[x]
+

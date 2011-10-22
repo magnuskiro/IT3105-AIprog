39e6a11eb3360d5306ba9c24a53b44d9a145d35b
Magnus Kirø <magnuskiro@gmail.com>
2011-10-22 11:16:15 +0200

    report update. created the codefiles and started commenting the code content

---
+++ SRS/code/classifier.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                   %
+%   IT3105 - Artificial Intelligence programming    %
+%   Sound Recognition System - SRS                  %
+%   Jan Alexander Bremnes and Magnus Kirø           %
+%   Oct - 2011                                      %
+%                                                   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% 
---
+++ SRS/code/main.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                   %
+%   IT3105 - Artificial Intelligence programming    %
+%   Sound Recognition System - SRS                  %
+%   Jan Alexander Bremnes and Magnus Kirø           %
+%   Oct - 2011                                      %
+%                                                   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% running the main program. 
+    % read the test file (input sound)
+    % call data prep on the input.
+    % instantiate the hmms (words)
+    % call classify and find out which word the sound represents.

cd89a5c2d93a06e602f93d2d11eca201fd558f06
Magnus Kirø <magnuskiro@gmail.com>
2011-10-22 11:55:00 +0200

    added more comments

--- SRS/code/fbhmm.m
+++ SRS/code/fbhmm.m
@@ -8,4 +8,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % contains the forward and backward hmm. and a function to calculate probability. 
-    %
+    % forward hmm, used to update the hmm, and used for identification of the sound. sums up the probabilities og calcProb.
+    % backward hmm, used for identification of the sound. 
+    % calcProb, calculates a partial probability of sound likelihood. 
